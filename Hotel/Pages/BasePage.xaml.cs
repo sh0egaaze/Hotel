874b78e8f0e8c3f307aedd32826f53af80099181
Arseniy <ars.sokolov102938475647@outlook.com>
2026-01-28 13:19:40 +0300

    Добавьте файлы проекта.

---
+++ Hotel/Pages/BasePage.xaml.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.Entity;
+
+namespace Hotel.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для BasePage.xaml
+    /// </summary>
+    public partial class BasePage : Page
+    {
+        public BasePage()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            lvClientDetails.Visibility = Visibility.Collapsed;
+            var context = Entities.GetContext();
+
+            var paymentBills = context.PaymentBill
+                .Include(b => b.Client)
+                .Include(b => b.Booking)
+                .Include(b => b.Booking.Room)
+                .Include(b => b.Booking.Room.Category)
+                .ToList();
+
+            var clientList = new List<dynamic>();
+
+            foreach (var bill in paymentBills)
+            {
+                string clientName = "Нет данных";
+                if (bill.Client != null)
+                {
+                    clientName = $"{bill.Client.lastName} {bill.Client.firstName} {bill.Client.middleName}";
+                }
+
+                string roomNum = "-";
+                string roomCat = "-";
+
+                if (bill.Booking != null && bill.Booking.Room != null)
+                {
+                    roomNum = bill.Booking.Room.number.ToString();
+                    if (bill.Booking.Room.Category != null)
+                    {
+                        roomCat = bill.Booking.Room.Category.name;
+                    }
+                }
+
+                string dateRange = "Не указано";
+                if (bill.Booking != null)
+                {
+                    dateRange = $"с {bill.Booking.dateFrom:dd.MM.yyyy}\nпо {bill.Booking.dateTill:dd.MM.yyyy}";
+                }
+
+                var services = (from sib in context.ServiceInBill
+                                join s in context.Service on sib.seviceId equals s.id
+                                where sib.billId == bill.id
+                                select s.name).ToList();
+
+                string servicesStr = services.Any() ? string.Join(", ", services) : "-";
+
+
+                clientList.Add(new
+                {
+                    ClientName = clientName,
+                    RoomNum = roomNum,
+                    RoomCat = roomCat,
+                    DateRange = dateRange,
+                    Services = servicesStr, 
+                    Amount = bill.amount
+                });
+            }
+
+            lvClientDetails.ItemsSource = clientList;
+
+            List<Booking> bookings = Entities.GetContext().Booking.ToList();
+
+            if (dpStart.SelectedDate != null)
+            {
+                DateTime dateStart = dpStart.SelectedDate.Value;
+                bookings = bookings.Where(b => b.dateFrom >= dateStart).ToList();
+            }
+            if (dpEnd.SelectedDate != null)
+            {
+                DateTime dateEnd = dpEnd.SelectedDate.Value;
+                bookings = bookings.Where(b => b.dateFrom <= dateEnd).ToList();
+            }
+
+            dgBookings.ItemsSource = bookings;
+            dgBookings.SelectedItem = null;
+        }
+
+        private void DeleteBooking()
+        {
+            Booking booking = dgBookings.SelectedItem as Booking;
+
+            if (booking != null)
+            {
+                var result = MessageBox.Show($"Удалить выбранное бронирование?", "Подтверждение удаления", 
+                    MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+                if (result == MessageBoxResult.Yes)
+                {
+                    Entities.GetContext().Booking.Remove(booking);
+                    Entities.GetContext().SaveChanges();
+
+                    MessageBox.Show("Бронирование удалено", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    dgBookings.SelectedItem = null;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Выберите запись для удаления", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void ShowBookingInfo()
+        {
+            spBooking.Visibility = Visibility.Visible;
+            lvClientDetails.Visibility = Visibility.Collapsed;
+            spBookingElements.IsEnabled = true;
+        }
+
+        private void ShowClientInfo()
+        {
+            spBooking.Visibility = Visibility.Collapsed;
+            lvClientDetails.Visibility = Visibility.Visible;
+            spBookingElements.IsEnabled = false;
+        }
+
+        private void CalcADR()
+        {
+            var context = Entities.GetContext();
+
+            decimal totalSellsAmount = context.PaymentBill.Sum(pb => pb.amount);
+            double totalNightsDouble = 0;
+
+            var bookings = context.Booking.ToList();
+
+            if (bookings.Any())
+            {
+                totalNightsDouble = bookings
+                    .Sum(b => (b.dateTill - b.dateFrom).TotalDays);
+            }
+
+            if (totalNightsDouble <= 0)
+            {
+                MessageBox.Show("Невозможно рассчитать ADR: нет данных о бронированиях или нулевое количество ночей");
+                return;
+            }
+
+            decimal totalNights = Convert.ToDecimal(totalNightsDouble);
+            decimal ADR = totalSellsAmount / totalNights;
+
+            MessageBox.Show($"Показатель ADR на основании данных об общей выручке {totalSellsAmount}" +
+                $" и общем количестве ночей проживания {totalNights} составляет {ADR:F2}");
+        }
+
+        private void CalcRoomBooked()
+        {
+            var context = Entities.GetContext();
+
+            int totalRooms = context.Room.Count();
+            double totalNightsDouble = 0;
+
+            var bookings = context.Booking.ToList();
+
+            if (bookings.Any())
+            {
+                totalNightsDouble = bookings
+                    .Sum(b => (b.dateTill - b.dateFrom).TotalDays);
+            }
+
+            if (totalNightsDouble <= 0)
+            {
+                MessageBox.Show("Невозможно рассчитать степень загрузки: нулевое количество ночей");
+                return;
+            }
+
+            decimal totalNights = Convert.ToDecimal(totalNightsDouble);
+
+            decimal loadPercent = totalNights / totalRooms * 100;
+
+            MessageBox.Show($"Показатель загрузки номерного фонда  на основании данных об общем количестве номеров {totalRooms}" +
+                $" и общем количестве ночей проживания {totalNights} составляет {loadPercent:F2}%");
+        }
+
+        private void btnFilter_Click(object sender, RoutedEventArgs e)
+        {
+            LoadData();
+        }
+
+        private void btnShowAll_Click(object sender, RoutedEventArgs e)
+        {
+            dpStart.SelectedDate = null;
+            dpEnd.SelectedDate = null;
+
+            LoadData();
+        }
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new BookingPage());
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            DeleteBooking();
+        }
+
+        private void btnnavToClient_Click(object sender, RoutedEventArgs e)
+        {
+            ShowClientInfo();
+        }
+
+        private void btnShowBooking_Click(object sender, RoutedEventArgs e)
+        {
+            ShowBookingInfo();
+        }
+
+        
+
+        private void btnRoomsBooked_Click(object sender, RoutedEventArgs e)
+        {
+            CalcRoomBooked();
+        }
+
+        private void btnShowADR_Click(object sender, RoutedEventArgs e)
+        {
+            CalcADR();
+        }
+    }
+}
---
+++ Hotel/Pages/BookingPage.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Hotel.Pages
+{
+    public partial class BookingPage : Page
+    {
+        public BookingPage()
+        {
+            InitializeComponent();
+
+            var clients = Entities.GetContext().Client.ToList();
+            cmbClient.ItemsSource = clients.Select(c => new
+            {
+                id = c.id,
+                fullName = c.lastName + " " + c.firstName + " " + c.middleName
+            }).ToList();
+
+            cmbRoom.ItemsSource = Entities.GetContext().Room.ToList();
+        }
+
+        private void CalculateAmount()
+        {
+            if (dpFrom.SelectedDate.HasValue && dpTill.SelectedDate.HasValue && cmbRoom.SelectedItem != null)
+            {
+                var room = cmbRoom.SelectedItem as Room;
+
+                var price = Entities.GetContext().RoomPrice
+                            .FirstOrDefault(p => p.categoryId == room.categoryId);
+
+                if (price != null)
+                {
+                    var days = (dpTill.SelectedDate.Value - dpFrom.SelectedDate.Value).TotalDays;
+                    lblAmount.Content = $"{price.price * (decimal)days}";
+                }
+                else
+                {
+                    lblAmount.Content = "Цена не найдена";
+                }
+            }
+        }
+
+        private void AddBooking()
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (cmbClient.SelectedItem == null)
+            {
+                errors.AppendLine("Укажите клиента");
+            }
+            if (cmbRoom.SelectedItem == null)
+            {
+                errors.AppendLine("Укажите номер");
+            }
+            if (dpFrom.SelectedDate == null)
+            {
+                errors.AppendLine("Укажите дату въезда");
+            }
+            if (dpTill.SelectedDate == null)
+            {
+                errors.AppendLine("Укажите дату выселения");
+            }
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                dynamic client = cmbClient.SelectedItem;
+
+                var room = cmbRoom.SelectedItem as Room;
+
+                var price = Entities.GetContext().RoomPrice
+                            .FirstOrDefault(p => p.categoryId == room.categoryId);
+
+                Booking booking = new Booking();
+
+                booking.clientId = client.id;
+                booking.Room = room;
+                booking.dateFrom = dpFrom.SelectedDate.Value;
+                booking.dateTill = dpTill.SelectedDate.Value;
+
+                var days = (dpTill.SelectedDate.Value - dpFrom.SelectedDate.Value).TotalDays;
+
+                if (price != null)
+                {
+                    booking.amount = price.price * (decimal)days;
+                }
+                else
+                {
+                    booking.amount = 0;
+                }
+
+                try
+                {
+                    Entities.GetContext().Booking.Add(booking);
+                    Entities.GetContext().SaveChanges();
+                    NavigationService.Navigate(new BasePage());
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            AddBooking();
+        }
+
+        private void dpFrom_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            CalculateAmount();
+        }
+
+        private void dpTill_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            CalculateAmount();
+        }
+
+        private void cmbRoom_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            CalculateAmount();
+        }
+    }
+}
